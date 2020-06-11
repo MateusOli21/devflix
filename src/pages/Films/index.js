c8c34edb2e5a8b8a519162c232064638d36fb85b
MateusOli21 <mateus.oli.abreu@hotmail.com>
2020-06-11 19:22:29 -0300

    Added some lists to films and series pages.

--- src/pages/Films/index.js
+++ src/pages/Films/index.js
@@ -7,7 +7,10 @@ function Films() {
   return (
     <>
       <FilmsPreview />
+      <DiscoverList isMovies genreId={18} label={"Drama"} />
+      <DiscoverList isMovies genreId={27} label={"Terror"} />
       <DiscoverList isMovies genreId={878} label={"Ficção científica"} />
+      <DiscoverList isMovies genreId={16} label={"Animação"} />
     </>
   );
 }
--- src/pages/Series/index.js
+++ src/pages/Series/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 import SeriesPreview from "../../components/SeriesPreview";
+import DiscoverList from "../../components/DiscoverList";
 
 function Series() {
   return (
     <>
       <SeriesPreview />
+      <DiscoverList isSeries genreId={28} label={"Ação"} />
+      <DiscoverList isSeries genreId={80} label={"Criminal"} />
+      <DiscoverList isSeries genreId={878} label={"Ficção científica"} />
+      <DiscoverList isSeries genreId={14} label={"Fantasia"} />
     </>
   );
 }
